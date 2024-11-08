8a9cf07f334fd37973ff5a20bb954db0d8c78b89
Nicolas Pereira <41456803+hqnicolas@users.noreply.github.com>
2024-11-07 21:22:57 -0300

    Java 23 Spring Initializr

---
+++ src/main/java/com/gerenciamento/backend/DonativosApplication.java
@@ -0,0 +1,13 @@
+package com.gerenciamento.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonativosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DonativosApplication.class, args);
+	}
+
+}
